612728d78e23971f311d6f8d8fd6adc4b5e0bb41
thennek0 <kennethbecaro0@gmail.com>
2024-05-04 21:09:50 +0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/web-portfolio-v3/",
+  // base: "/web-portfolio-v3/",
+  base: "/",
 });
